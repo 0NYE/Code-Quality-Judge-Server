eae00e017ff5aa14dcb70c844fcd6da3dab68053
jjiny0716 <jjiny3773@naver.com>
2023-05-03 23:33:00 +0900

    feat: express 의존성 추가 및 명령어 수정
    - express를 external로 추가

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("*", (req: Request, res: Response) => {
+  res.status(404).send("not found");
+});
+
+app.listen("3000", () => {
+  console.log(`Server listening on port: 3000`);
+});
