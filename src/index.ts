795843a79bca129cdd50bea888ba92326bb98d08
jjiny0716 <jjiny3773@naver.com>
2023-05-11 17:35:59 +0900

    feat: 구현한 api url 매핑

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import cors from "cors";
 import express, { Request, Response } from "express";
 
+import judgeRouter from "@/routers/judgeRouter";
+
 const app = express();
 
 app.use(cors());
+
+app.use("/api/judge", judgeRouter)
 
 app.get("*", (req: Request, res: Response) => {
   res.status(404).send("not found");
