7822847a85208bb4ea4d5481c00fb89ae850bf8d
jjiny0716 <jjiny3773@naver.com>
2023-05-11 23:52:06 +0900

    feat: judge webapp api 구현 - lighthouse 결과를 제공

--- src/controllers/judgeController.ts
+++ src/controllers/judgeController.ts
@@ -1,7 +1,28 @@
+import * as chromeLauncher from "chrome-launcher";
 import { Request, Response } from "express";
 
-export const judgeWebApp = (req: Request, res: Response) => {
-  res.status(200).json({
-    result: "your code is good",
+export const judgeWebApp = async (req: Request, res: Response) => {
+  const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
+  const lighthouse = (await import("lighthouse")).default;
+  
+  const runnerResult = await lighthouse(`http://localhost:3000/webapp/${req.id}.html`, {
+    logLevel: "info",
+    port: chrome.port,
+  }, {
+    extends: "lighthouse:default",
+    settings: {
+      onlyCategories: ["accessibility"],
+    }
+  });
+  
+  if (!runnerResult) {
+    return res.status(500).json({
+      result: "fail",
+    })
+  } 
+
+  return res.status(200).json({
+    result: "sucess",
+    report: JSON.stringify(runnerResult.lhr),
   })
 };
